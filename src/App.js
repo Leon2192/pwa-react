a254e403b49251a691ce10fce98b377bcbaaa1a1
leon_2192 <leon.strokes@gmail.com>
2023-08-08 09:04:50 -0300

    fix app e index

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import LandingEN from "./components/LandingEN";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/Main";
 import { ChakraProvider } from "@chakra-ui/react"; // Cambia la importación aquí
 
 function App() {
-
-  useEffect(() => {
-    navigator.serviceWorker.addEventListener('message', (event) => {
-      if (event.data && event.data.type === 'showInstallBanner') {
-        // Mostrar el banner de instalación en la aplicación
-        const installButton = document.createElement('button');
-        installButton.innerText = 'Instalar';
-        installButton.addEventListener('click', () => {
-          // Obtener el evento de instalación diferida y ejecutarlo
-          const deferredPrompt = window.deferredPrompt;
-          if (deferredPrompt) {
-            deferredPrompt.prompt();
-          }
-        });
-
-        // Agregar el botón al DOM
-        document.body.appendChild(installButton);
-      }
-    });
-  }, []);
-
   return (
     <ChakraProvider>
       <BrowserRouter>
@@ -39,3 +18,4 @@ function App() {
 }
 
 export default App;
+

58f15a2cc4afddf50a1b39c9fc2396a38d507563
leon_2192 <leon.strokes@gmail.com>
2023-08-08 09:47:02 -0300

    fix app e index

--- src/index.js
+++ src/index.js
@@ -15,9 +15,22 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.register();
-serviceWorkerRegistration.register();
-// ^^^ No es necesario pasar la configuración adicional a la función register()
+///serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({
+  onNotificationClick: (notification) => {
+    // Lógica para manejar el clic en la notificación
+    // Por ejemplo, podrías redirigir a una página específica
+    window.location.href = notification.data.url;
+  },
+  onOfflineReady: () => {
+    // Lógica para manejar cuando la app está lista para usarse en línea
+    console.log('La aplicación está lista para usarse sin conexión.');
+  },
+  onInstalled: () => {
+    // No es necesario agregar nada aquí, ya que la lógica del banner de instalación
+    // se manejará en el Service Worker
+  },
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
